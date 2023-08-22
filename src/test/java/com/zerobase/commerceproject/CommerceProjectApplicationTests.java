977402a75e01022755a70b8767645028185d51e4
lkjh8sda <lkjh8sda@naver.com>
2023-08-22 20:41:00 +0900

    기본 세팅

---
+++ src/test/java/com/zerobase/commerceproject/CommerceProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zerobase.commerceproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommerceProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
